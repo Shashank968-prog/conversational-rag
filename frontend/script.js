fc27939eeba57f8aca2127c327c8adba676ef4c4
Shashank968-prog <ksshashank693@gmail.com>
2026-08-25 14:43:21 +0530

    Fix chat fetch URL for production deploy

--- frontend/script.js
+++ frontend/script.js
@@ -26,7 +26,7 @@ async function sendQuestion() {
     try {
 
         const response = await fetch(
-            "http://127.0.0.1:8000/chat",
+            "/chat",
             {
                 method: "POST",
 
